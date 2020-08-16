1086fb8604f2186d30c86a14862b00e1fd23ab68
namseokyoo <namseok.yoo@gmail.com>
2020-08-17 01:23:25 +0900

    make model DB table

--- shoppingmall/shop/models.py
+++ shoppingmall/shop/models.py
@@ -1,3 +1,27 @@
+import datetime
+import uuid
 from django.db import models
+from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField(default=0)
+    image1 = models.ImageField(blank=False)
+    image2 = models.ImageField(blank=True)
+    image3 = models.ImageField(blank=True)
+    description = models.CharField(max_length=100)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    name = models.CharField(max_length=5)
+    order_number = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.PositiveIntegerField(default=0)
+    phone_number = PhoneNumberField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    email= models.EmailField(max_length=50)
+    address = models.CharField(max_length=100)
+    delivered = models.BooleanField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
