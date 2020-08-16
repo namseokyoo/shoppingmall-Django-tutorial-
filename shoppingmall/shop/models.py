0d223ac689fa7f878ed4bfc39bf119fa8c723aec
namseokyoo <namseok.yoo@gmail.com>
2020-08-17 08:52:00 +0900

    fix model.py name label

--- shoppingmall/shop/models.py
+++ shoppingmall/shop/models.py
@@ -7,17 +7,18 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class Item(models.Model):
-    name = models.CharField(max_length=20)
+    Item_text = models.CharField(max_length=20)
     price = models.PositiveIntegerField(default=0)
     image1 = models.ImageField(blank=False)
     image2 = models.ImageField(blank=True)
     image3 = models.ImageField(blank=True)
     description = models.CharField(max_length=100)
     post_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.Item_text
 
 class Order(models.Model):
-    name = models.CharField(max_length=5)
-    order_number = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Order_text = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,)
     amount = models.PositiveIntegerField(default=0)
     phone_number = PhoneNumberField()
     order_date = models.DateTimeField(auto_now_add=True)
@@ -25,3 +26,5 @@ class Order(models.Model):
     address = models.CharField(max_length=100)
     delivered = models.BooleanField()
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.Order_text
