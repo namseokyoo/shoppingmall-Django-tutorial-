3220fc7fb3e05b2064b0c045849ae945fb634ffe
namseokyoo <namseok.yoo@gmail.com>
2020-08-17 13:16:15 +0900

    Add page urls

--- shoppingmall/shop/urls.py
+++ shoppingmall/shop/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: shop/     (main item list page)
+    path('', views.index, name='index'),
+    # ex: shop/2    (item-order page)
+    path('<int:item_id>/',views.itemdetail, name = 'itemdetail'),
+    # ex: shop/orderlist    (order list page)
+    path('orderlist/',views.orderlist,name ='orderlist')
 ]
--- shoppingmall/shop/views.py
+++ shoppingmall/shop/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from shop.models import Item, Order
 # Create your views here.
 
 def index(request):
     context={}
     return render(request,'index.html',context)
+
+def itemdetail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    return render(request, 'itemdetail.html',{'item':item})
+
+def orderlist(request):
+    return render(request, 'orderlist.html')
